d3ab4d19e8b3ac13d54aa2138bf1d987f493fd3a
prasadV05 <135524702+prasadV05@users.noreply.github.com>
2023-07-08 01:42:33 +0530

    updated create views in views and urls

--- todo_app/views.py
+++ todo_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def list_view(request):
     pass
+def create_view(request):
+    pass
 def update_view(request, todo_id):
     pass
 def delete_view(request, todo_id):
