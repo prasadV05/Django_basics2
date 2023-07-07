9ba44d9673396559769c4ac128f33b291e0ad89c
prasadV05 <135524702+prasadV05@users.noreply.github.com>
2023-07-07 18:35:15 +0530

    template changes

--- index_page_app/views.py
+++ index_page_app/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index_page_view(request):
     data = {
+        "show_data" : True,
         "name" : "Varsha",
-        "course" : "Python Backend Development Live"
+        "course" : "Python Backend Development Live",
+        "list" : [1,2,3,4]
     }
     return render(request, "index.html",context=data)
