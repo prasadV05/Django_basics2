cdeb4cfebc21cc7830fb70eac549d15cf4eb8b45
prasadV05 <135524702+prasadV05@users.noreply.github.com>
2023-07-07 21:13:09 +0530

    added json in view

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from my_app.views import index_view
-
+from my_app.views import index_view, my_app_detail_views
 urlpatterns = [
     path("index/", index_view),
+    path("detail/<int:my_app_id>", my_app_detail_views)
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from my_app.models import My_app
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def index_view(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+def my_app_detail_views(request, my_app_id):
+    try:
+        my_app_object= My_app.objects.get(pk=my_app_id)
+    except My_app.DoesNotExist:
+        return JsonResponse({"error" : True, "message" : "My_app does not exist"})
+    return JsonResponse({
+        "id" : my_app_object.id,
+        "name" : my_app_object.name,
+        "age" : my_app_object.age
+    })
