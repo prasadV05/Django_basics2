e66374a7ac142dc86eb8c45a75326ef8a77fb418
prasadV05 <135524702+prasadV05@users.noreply.github.com>
2023-07-08 01:13:08 +0530

    todo app added

--- project2/settings.py
+++ project2/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_app','index_page_app'
+    'my_app','index_page_app',
+    'todo_app'
 ]
 
 MIDDLEWARE = [
